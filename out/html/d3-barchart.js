672013fc641ba037f77014761c1dd3c16590c41c
Lord910 <34657679+Lord910@users.noreply.github.com>
2024-10-10 19:31:42 +0000

    barchart test (again)

--- out/html/d3-barchart.js
+++ out/html/d3-barchart.js
@@ -1,54 +1,35 @@
-/*
- * MIT License
- * © Copyright 2016 - Geoffrey Brossard (me@geoffreybrossard.fr)
- */
-
-d3.barchart = function() {
-    /* params */
-    var width,
-        height,
+d3.simpleBarchart = function() {
+    var width = 500,
+        height = 300,
         margin = {top: 20, right: 30, bottom: 40, left: 40};
 
-    /* animations */
-    var enter = {
-            "fromBottom": true
-        },
-        update = {
-          'animate': true,
-        },
-        exit = {
-            "toBottom": true
-        };
-
-    /* events */
-    var barchartDispatch = d3.dispatch("click", "dblclick", "mousedown", "mouseenter",
-        "mouseleave", "mousemove", "mouseout", "mouseover", "mouseup", "touchcancel", "touchend",
-        "touchmove", "touchstart");
-
-    function barchart(data) {
-        console.log(data);
-        data.each(function(d) {
-
-            // if user did not provide, fill the svg:
-            width = width ? width : this.getBoundingClientRect().width;
-            height = height ? height : this.getBoundingClientRect().height;
-
-            var svg = d3.select(this);
-
-            /* init the svg */
-            svg.attr("width", width)
-               .attr("height", height);
+    function barchart(selection) {
+        selection.each(function() {
+            var svg = d3.select(this)
+                        .attr("width", width)
+                        .attr("height", height);
 
             var chartWidth = width - margin.left - margin.right;
             var chartHeight = height - margin.top - margin.bottom;
 
-            var x = d3.scaleBand()
-                      .domain(d.map(function(p) { return p.id; }))
-                      .range([0, chartWidth])
-                      .padding(0.1);
+            var data = [];
+            for (var i = 1; i <= 5; i++) {
+                data.push({group: 'Group ' + i, subgroup: 'A', value: Math.floor(Math.random() * 10) + 1});
+                data.push({group: 'Group ' + i, subgroup: 'B', value: Math.floor(Math.random() * 10) + 1});
+            }
+
+            var x0 = d3.scaleBand()
+                       .domain(data.map(d => d.group))
+                       .range([0, chartWidth])
+                       .padding(0.1);
+
+            var x1 = d3.scaleBand()
+                       .domain(['A', 'B'])
+                       .range([0, x0.bandwidth()])
+                       .padding(0.05);
 
             var y = d3.scaleLinear()
-                      .domain([0, d3.max(d, function(p) { return p.seats; })])
+                      .domain([0, 10])
                       .nice()
                       .range([chartHeight, 0]);
 
@@ -58,59 +39,25 @@ d3.barchart = function() {
             g.append("g")
              .attr("class", "axis axis--x")
              .attr("transform", "translate(0," + chartHeight + ")")
-             .call(d3.axisBottom(x));
+             .call(d3.axisBottom(x0));
 
             g.append("g")
              .attr("class", "axis axis--y")
-             .call(d3.axisLeft(y).ticks(10, "s"));
+             .call(d3.axisLeft(y).ticks(10));
 
-            /* create bars */
-            var bars = g.selectAll(".bar")
-                        .data(d)
-                        .enter().append("rect")
-                        .attr("class", "bar")
-                        .attr("x", function(p) { return x(p.id); })
-                        .attr("y", function(p) { return y(p.seats); })
-                        .attr("width", x.bandwidth())
-                        .attr("height", function(p) { return chartHeight - y(p.seats); })
-                        .attr("fill", function(p) { return p.color; });
+            var group = g.selectAll(".group")
+                         .data(data)
+                         .enter().append("g")
+                         .attr("transform", d => "translate(" + x0(d.group) + ",0)");
 
-            /* animation adding bars to the barchart */
-            if (enter.fromBottom) {
-                bars.attr("y", chartHeight)
-                    .attr("height", 0)
-                    .transition()
-                    .duration(1000)
-                    .attr("y", function(p) { return y(p.seats); })
-                    .attr("height", function(p) { return chartHeight - y(p.seats); });
-            }
-
-            /* bars catch mouse and touch events */
-            for (var evt in barchartDispatch._) {
-                (function(evt){
-                    bars.on(evt, function(e) { barchartDispatch.call(evt, this, e); });
-                })(evt);
-            }
-
-            /* animation updating bars in the barchart */
-            if (update.animate) {
-              bars.transition()
-                  .duration(1000)
-                  .attr("y", function(p) { return y(p.seats); })
-                  .attr("height", function(p) { return chartHeight - y(p.seats); });
-            }
-
-            /* animation removing bars from the barchart */
-            if (exit.toBottom) {
-                bars.exit()
-                    .transition()
-                    .duration(1000)
-                    .attr("y", chartHeight)
-                    .attr("height", 0)
-                    .remove();
-            } else {
-                bars.exit().remove();
-            }
+            group.selectAll("rect")
+                 .data(d => [d])
+                 .enter().append("rect")
+                 .attr("class", "bar")
+                 .attr("x", d => x1(d.subgroup))
+                 .attr("y", d => y(d.value))
+                 .attr("width", x1.bandwidth())
+                 .attr("height", d => chartHeight - y(d.value));
         });
     }
 
@@ -131,34 +78,6 @@ d3.barchart = function() {
         margin = value;
         return barchart;
     };
-
-    barchart.enter = {
-        fromBottom: function (value) {
-            if (!arguments.length) return enter.fromBottom;
-            enter.fromBottom = value;
-            return barchart.enter;
-        }
-    };
-
-    barchart.update = {
-      animate: function(value) {
-        if (!arguments.length) return update.animate;
-        update.animate = value;
-        return barchart.update;
-      }
-    }
-
-    barchart.exit = {
-        toBottom: function (value) {
-            if (!arguments.length) return exit.toBottom;
-            exit.toBottom = value;
-            return barchart.exit;
-        }
-    };
-
-    barchart.on = function(type, callback) {
-        barchartDispatch.on(type, callback);
-    }
 
     return barchart;
 }
